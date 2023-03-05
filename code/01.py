23260c1dbe7fd4513e0b073671e43f3b1ce4bf6c
Pritom Saha <58429037+pritomsh@users.noreply.github.com>
2023-03-05 20:30:13 +0600

    using print function

---
+++ code/01.py
@@ -0,0 +1,9 @@
+#how to print 
+print('Hello World')
+
+#add new line use \n in print function
+print('Pritom Saha \nData Analyst')
+
+
+# to ignore special characters in python add a backslash ( \ ) before the character
+print('He\'s name is Pritom')
