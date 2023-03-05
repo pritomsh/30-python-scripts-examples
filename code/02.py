2e370c813408d53c1340172631fae30e28b0000e
Pritom Saha <58429037+pritomsh@users.noreply.github.com>
2023-03-05 21:55:49 +0600

    q02 add 2 string

---
+++ code/02.py
@@ -0,0 +1,12 @@
+#  add 2 string
+# make full name using first name and last name
+
+f_name = "Pritom"
+l_name = "Saha"
+full_name = f_name+ " " + l_name
+print(full_name)
+
+'''
+Output:
+Pritom Saha
+'''
