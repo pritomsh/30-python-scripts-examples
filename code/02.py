9a703a9726cf26ffa4f36fc2b576a423fa2a44c3
Pritom Saha <58429037+pritomsh@users.noreply.github.com>
2023-03-05 22:03:19 +0600

    using format to add string

--- code/02.py
+++ code/02.py
@@ -1,12 +1,30 @@
-#  add 2 string
+#  add More than two strings can be concatenated using ‘+’ operator.
 # make full name using first name and last name
 
 f_name = "Pritom"
 l_name = "Saha"
 full_name = f_name+ " " + l_name
-print(full_name)
+print("Full Name :",full_name)
 
 '''
 Output:
 Pritom Saha
+'''
+
+'''
+Using format()
+The format() is a string formatting function used to concatenate two strings.
+''' 
+
+namef="Himel"
+namel="Saha"
+fname="Full name : {} {}".format(namef,namel)
+
+print(fname)
+
+'''
+Output:
+
+Full Name : Pritom Saha
+Full name : Himel Saha
 '''
