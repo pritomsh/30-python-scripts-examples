45556b8acb871bc9e9be828dabdcf08bed3eb1c6
Pritom Saha <58429037+pritomsh@users.noreply.github.com>
2023-03-07 23:51:55 +0600

    example 09 while loop

---
+++ code/09.py
@@ -0,0 +1,9 @@
+# Initialize counter
+i = input('How much number you want to sum')
+sum = 0
+# Iterate the loop 5 times
+while counter < 6:
+    # Print the counter value
+    print("The current counter value: %d" % counter)
+    # Increment the counter
+    counter = counter + 1
